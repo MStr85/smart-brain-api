671db32c23f07f94824b7b78e56cd1e8a59e6b4e
Marek <marek.strzalkowski@gmail.com>
2021-08-01 14:31:08 +0200

    fix 4

--- server.js
+++ server.js
@@ -26,9 +26,20 @@ const db = knex({
 });
 */
 
+const whitelist = ['https://arcane-dawn-52860.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 /* const database = {
 	users: [
