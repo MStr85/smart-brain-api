1df1586a395f48701c8254ad0e62c84413a80691
Marek <marek.strzalkowski@gmail.com>
2021-08-01 14:41:19 +0200

    fix 6

--- server.js
+++ server.js
@@ -26,20 +26,9 @@ const db = knex({
 });
 */
 
-const whitelist = ['http://localhost:3000']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
-
 const app = express();
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 /* const database = {
 	users: [
